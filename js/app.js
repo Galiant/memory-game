ba3962d990719acc29e8eac07fde96cecfa73733
Galiant <antonijodev@gmail.com>
2018-04-29 17:02:50 +0100

    Open, show and match functionality. Shuffle functionality on window load.

--- js/app.js
+++ js/app.js
@@ -1,45 +1,97 @@
 /*
  * Create a list that holds all of your cards
- *
  */
-let cards = document.getElementsByClassName('card')
+let cards = document.getElementsByClassName('card');
 
-let allCards = [...cards]
+let allCards = [...cards];
 
-let openCard = []
+let openCard = [];
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+const deck = document.querySelector(".deck");
 
-const deck = document.querySelector('.deck')
-
-// this adds the clicked card to the array, only allowing 2 cards to be stored at once
+// Add the clicked card to the array, only allowing 2 cards to be stored at once
 function showCard() {
-  this.className += 'show open'
-  openCard.push(this)
-  var length = openCard.length
+  this.className += ' show open';
+  openCard.push(this);
+  var length = openCard.length;
   if (length === 2) {
-    compareCards()
+    compareCards();
   }
 };
 
-// this adds the event listener to all cards
+// Add the event listener to all cards 
 for (i = 0; i < allCards.length; i++) {
-  card = allCards[i]
-  card.addEventListener('click', showCard)
+  card = allCards[i];
+  card.addEventListener('click', showCard);
 }
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+// Keep matched cards open
+function matchCard() {
+  openCard[1].classList.add('match');
+  openCard[0].classList.add('match');
+  if (openCard > 2) {
+    openCard = [];
+  }
+}
+
+// Add class to opened wrong card
+function wrongCard() {
+  for (let n of openCard) {
+    n.classList.add('wrong');
+  }
+};
+
+// Hide opened card if they don't match
+function hideCards() {
+  for (let n of openCard) {
+    n.classList.remove('open', 'show', 'wrong');
+  }
+};
+
+// Compare open cards and do action on them
+function compareCards() {
+  if (openCard[0].innerHTML === openCard[1].innerHTML) {
+    matchCard();
+    openCard = [];
+  } else {
+    wrongCard();
+    setTimeout(function () {
+      hideCards();
+      openCard = [];
+    }, 700);
+  };
+};
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue, randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+};
+
+// Shuffle cards in deck and remove all classes from each card
+function shuffleCards() {
+  for (var j = 0; j < cards.length; j++) {
+    cards[j].classList.remove("open", "show", "match");
+  };
+
+  allCards = shuffle(allCards);
+  for (var i = 0; i < allCards.length; i++) {
+    deck.innerHTML = "";
+    for (const e of allCards) {
+      deck.appendChild(e);
+    }
+  }
+};
+
+// Shuffle cards in deck on window load
+window.addEventListener('load', shuffleCards());
